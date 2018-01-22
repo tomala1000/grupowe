456f312d40fadb12dd26ed4a17a2fbda0946e1e7
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2018-01-22 19:24:23 +0100

    password

--- src/main/java/org/sda/servlets/domain/Password.java
+++ src/main/java/org/sda/servlets/domain/Password.java
@@ -11,9 +11,34 @@ public class Password {
     @GeneratedValue
     private Long id;
 
+    private String value;
+
     @OneToOne(optional = false)
     @JoinColumn(name = "user_id", unique = true)
     @NotNull
     private User user;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
