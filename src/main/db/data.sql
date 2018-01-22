ab82c5ba2daa1eb58a7e7a5d24953329a25c0966
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2018-01-22 18:23:25 +0100

    sql

---
+++ src/main/db/data.sql
@@ -0,0 +1,11 @@
+insert into course(id, name) values(10001, 'Testowo');
+insert into course(id, name) values(10002, 'Testowo2');
+insert into course(id, name) values(10003, 'Testowo3');
+insert into course(id, name) values(10004, 'Testowo4');
+insert into course(id, name) values(10005, 'Testowo5');
+
+insert into "user"(id, firstname, lastname, email) values(20001, 'Eddie', 'Larrison','cos@o2.pl');
+insert into "user"(id, firstname, lastname, email) values(20002, 'Scala', 'Johansson','cos@o2.pl');
+insert into "user"(id, firstname, lastname, email) values(20003, 'Lenny', 'Linux','cos@o2.pl');
+insert into "user"(id, firstname, lastname, email) values(20004, 'Wiliam', 'Windows','cos@o2.pl');
+
--- src/main/db/database.sql
+++ src/main/db/database.sql
@@ -3,6 +3,15 @@ CREATE ROLE sda_user LOGIN
   NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
 
 CREATE DATABASE sda_db
+    WITH
+    OWNER = sda_user
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+CREATE DATABASE sda_db
   WITH OWNER = sda_user
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
