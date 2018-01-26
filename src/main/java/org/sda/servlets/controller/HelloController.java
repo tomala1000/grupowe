7bdcd9ba18eb40a7f7a8f3798be7e175e3b26ec6
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2018-01-26 17:45:40 +0100

    spring

--- src/main/java/org/sda/servlets/controller/HelloController.java
+++ src/main/java/org/sda/servlets/controller/HelloController.java
@@ -1,16 +1,31 @@
 package org.sda.servlets.controller;
 
+import org.sda.servlets.domain.User;
+import org.sda.servlets.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.Resource;
+
 @Controller
 public class HelloController {
+
+    @Resource
+    private UserRepository userRepository;
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "hello";
     }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String register(@ModelAttribute User user, ModelMap model) {
+        model.addAttribute("message", "Hello Spring MVC Framework!");
+        return "hello";
+    }
+
 }
--- src/main/java/org/sda/servlets/filter/AuthorizationFilter.java
+++ src/main/java/org/sda/servlets/filter/AuthorizationFilter.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebFilter("/*")
+//@WebFilter("/*")
 public class AuthorizationFilter implements Filter {
 
     private List<String> whiteList = new ArrayList<>();
@@ -21,7 +21,7 @@ public class AuthorizationFilter implements Filter {
         whiteList.add("/pages/login.jsp");
         whiteList.add("/login");
         whiteList.add("/pages/registerUser.jsp");
-        whiteList.add("/registration");
+        whiteList.add("/register");
     }
 
     @Override
